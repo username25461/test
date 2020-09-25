b0fff7d9e9552a3916616bc58466e377d72d2c3c
araksys64 <57962971+araksys64@users.noreply.github.com>
2020-09-26 01:13:11 +0400

    Add files via upload

---
+++ utm.js
@@ -0,0 +1,17 @@
+    <script type="text/javascript">
+      
+document.addEventListener("DOMContentLoaded", function() {
+ var hvost = window.location.search.substring(1);
+ 
+ var elements = document.getElementsByTagName('iframe');
+
+ for (var i = 0; i < elements.length; i++) {
+  var newHref = elements[i].src;
+  if (newHref.indexOf("#")!=-1) continue;
+  if (newHref.indexOf("?")==-1) newHref += "?";
+  else newHref += "&";
+  if (hvost!="") elements[i].src = newHref + hvost;
+ }
+});
+                                    
+</script>
